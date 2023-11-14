dae3618dbdee948ed0089ad8286c04da96842b48
GABRIELA <gabriela.silva@sou.inteli.edu.br>
2023-11-14 13:29:02 -0300

    Alterações finais

--- api.py
+++ api.py
@@ -3,14 +3,12 @@
 # pip install datetime
 # pip install pytz
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 import requests
 import pytz
-
-
 
 app = Flask(__name__)
 
@@ -21,8 +19,7 @@ db = SQLAlchemy(app)
 brasil_timezone = pytz.timezone('America/Sao_Paulo')
 
 def get_brasil_datetime():
-   return datetime.now(brasil_timezone).strftime("%Y-%m-%d %H:%M:%S")
-
+   return datetime.now(brasil_timezone)
 
 class WeatherData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,42 +29,44 @@ class WeatherData(db.Model):
     uso = db.Column(db.String(100))
 
 def funcao_etl():
-    
-    api_key = 'd13a18996616513b72952410f7ecd170'
-    cidades = ['São Paulo', 'Acre', 'Rio Grande do Sul', 'Alagoas', 'Brasília']
-    
-    lista_dados_climaticos = []  
-    
-    for city in cidades:
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-        response = requests.get(url)
-        print(response.json())
+   
+   api_key = 'd13a18996616513b72952410f7ecd170'
+   cidades = ['São Paulo', 'Brasília']
+   
+   lista_dados_climaticos = [] 
 
-        if response.status_code == 200:
-            weather_data = response.json()
-            
-            weather_data_json = json.dumps(weather_data)
-            
-            weather_entry = WeatherData(
-                data_tipo='openweather',
-                valores=weather_data_json,
-                uso='previsao_climatica'
-            )
-            
-            db.session.add(weather_entry)
-            db.session.commit()
-            
-            lista_dados_climaticos.append(weather_data_json)  
-            
-        else:
-            return f'Erro na extração dos dados da API OpenWeather para {city}', 500
+   for city in cidades:
+       url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+       response = requests.get(url)
 
-    return lista_dados_climaticos
+       print(f'Response status code: {response.status_code}')
+       print(f'Response content: {response.json()}')
+
+       if response.status_code == 200:
+           weather_data = response.json()
+           
+           weather_data_json = json.dumps(weather_data)
+           
+           weather_entry = WeatherData(
+               data_tipo='openweather',
+               valores=weather_data_json,
+               uso='previsao_climatica'
+           )
+
+           lista_dados_climaticos.append(weather_entry) 
+           
+       else:
+           return f'Erro na extração dos dados da API OpenWeather para {city}', 500
+
+   db.session.add_all(lista_dados_climaticos)
+   db.session.commit()
+
+   return lista_dados_climaticos
 
 @app.route('/weather_data', methods=['GET'])
 def display_weather_data():
-    weather_data = WeatherData.query.all()
-    return render_template('weather_table.html', weather_data=weather_data)
+   weather_data = WeatherData.query.all()
+   return render_template('weather_table.html', weather_data=weather_data)
 
 if __name__ == '__main__':
     with app.app_context():
--- teste.py
+++ teste.py
@@ -20,21 +20,16 @@ def client():
        db.session.remove()
        db.drop_all()
 
-
-
-
 @responses.activate
 def test_etl_route(client):
    responses.add(responses.GET, 'https://api.openweathermap.org/data/2.5/weather',
                 json={'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01d'}]},
                 status=200)
 
-   response = client.get('/etl')
+   response = client.get('/')
    assert response.status_code == 200
    assert 'Dados extraídos e armazenados no banco de dados' in response.data.decode()
    assert weather_data[4].uso == 'previsao_climatica'
-
-
 
    weather_data = WeatherData.query.all()
    assert len(weather_data) == 3
